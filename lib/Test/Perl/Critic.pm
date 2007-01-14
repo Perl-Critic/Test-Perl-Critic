af6d945c362e61f71ca766a1f36e9afa9bd0cc2a
Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
2007-01-14 22:17:22 +0000

    Fixed errors in new SYNOPSIS

--- lib/Test/Perl/Critic.pm
+++ lib/Test/Perl/Critic.pm
@@ -147,9 +147,9 @@ Recommended usage for CPAN distros:
 
   use strict;
   use warnings;
+  use File::Spec;
+  use Test::More;
   use English qw(-no_match_vars);
-  use File::Spec qw();
-  use Test::More qw(plan);
 
   if ( not $ENV{TEST_AUTHOR} ) {
       my $msg = 'Author test.  Set $ENV{TEST_AUTHOR} to a true value to run.';
