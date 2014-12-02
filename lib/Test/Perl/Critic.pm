d10ddcac6cec4359c3c2d87b0a84be575c421de4
Jeffrey Ryan Thalhammer <jeff@thaljef.org>
2014-12-02 13:37:37 -0800

    Update copyrights.

--- lib/Test/Perl/Critic.pm
+++ lib/Test/Perl/Critic.pm
@@ -392,11 +392,11 @@ documentation for Test::Perl::Critic.  Thanks, Andy.
 
 =head1 AUTHOR
 
-Jeffrey Ryan Thalhammer <jeff@imaginative-software.com>
+Jeffrey Ryan Thalhammer <jeff@thaljef.org>
 
 =head1 COPYRIGHT
 
-Copyright (c) 2005-2009 Imaginative Software Systems.  All rights reserved.
+Copyright (c) 2005-2014 Jeffrey Ryan Thalhammer.
 
 This program is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.  The full text of this license
